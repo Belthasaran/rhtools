076a967e34ccf23054ed12b326d40fd8497a6e6e
Anonymous <>
2023-12-30 05:24:37 -0600

    message text

--- chatbot/ccinteract.py
+++ chatbot/ccinteract.py
@@ -134,11 +134,11 @@ class CrowdInteract():
                 json = requestObject, 
                 headers = self.getRequestHeaders(cc_auth_token))
         if response.status_code == 200:
-            self.logger.info('ccinteract:getSessionInfo success')
+            self.logger.info('ccinteract:requestEffect success')
             self.logger.debug(f'Content = {response.content}')
             return json.loads(response.content)['result']['data']['effectRequest']
         else:
-            self.logger.error(f'ccinteract:getSessionInfo fail status={response.status_code} {response.text}')
+            self.logger.error(f'ccinteract:requestEffect fail status={response.status_code} {response.text}')
             return None
 
 #
